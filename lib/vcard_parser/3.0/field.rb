4bd119f18604d3f71f549b48ed11d6be5f36a350
Julien Ammous <schmurfy@gmail.com>
2013-05-21 15:32:35 +0200

    fixed regexp

--- lib/vcard_parser/3.0/field.rb
+++ lib/vcard_parser/3.0/field.rb
@@ -7,8 +7,8 @@ module VCardParser
       attr_reader :name, :group, :value, :params
       
       FORMAT = /^
-          (?:(?<group>[a-zA-Z0-9-]+)\.)?
-          (?<name>[a-zA-Z0-9-\/]+)
+          (?:(?<group>[a-zA-Z0-9\-]+)\.)?
+          (?<name>[a-zA-Z0-9\-\/]+)
           (?<params> (?:;[A-Za-z]+=[^;]+)+ )?
           :(?<value>.*)
         $/x
